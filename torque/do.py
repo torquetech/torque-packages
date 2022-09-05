26734f5ada7d0f7bc3bf47a6dcf295c778501ef4
Jacob Zeba <jacob@torquetech.dev>
2022-09-05 14:40:51 -0400

    Implement digital ocean provider engine

--- torque/do.py
+++ torque/do.py
@@ -4,6 +4,8 @@
 
 """TODO"""
 
+import typing
+
 from torque import v1
 from torque import dolib
 
@@ -12,8 +14,18 @@ class Provider(v1.provider.Provider):
     """TODO"""
 
     _CONFIGURATION = {
-        "defaults": {},
-        "schema": {}
+        "defaults": {
+            "endpoint": "https://api.digitalocean.com",
+            "token": "invalid_token",
+            "region": "nyc1",
+            "quiet": True
+        },
+        "schema": {
+            "endpoint": str,
+            "token": str,
+            "region": str,
+            "quiet": bool
+        }
     }
 
     @classmethod
@@ -33,14 +45,84 @@ class Provider(v1.provider.Provider):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self._current_state = {}
+        self._new_state = {}
+
+    def _connect(self) -> dolib.Client:
+        """TODO"""
+
+        return dolib.connect(self.configuration["endpoint"],
+                             self.configuration["token"])
+
+    def _load_state(self, context: v1.deployment.Context) -> dict[str, object]:
+        """TODO"""
+
+        with context as ctx:
+            self._current_state = ctx.get_data("state", self, "state") or {}
+
+
+    def _store_state(self, context: v1.deployment.Context):
+        """TODO"""
+
+        with context as ctx:
+            ctx.set_data("state", self, "state", self._current_state)
+
     def on_apply(self, context: v1.deployment.Context, dry_run: bool):
         """TODO"""
+
+        client = self._connect()
+        self._load_state(context)
+
+        try:
+            dolib.apply(client,
+                        self._current_state,
+                        self._new_state,
+                        self.configuration["quiet"])
+
+        finally:
+            self._store_state(context)
 
     def on_delete(self, context: v1.deployment.Context, dry_run: bool):
         """TODO"""
 
+        client = self._connect()
+        self._load_state(context)
+
+        try:
+            dolib.apply(client,
+                        self._current_state,
+                        self._new_state,
+                        self.configuration["quiet"])
+
+        finally:
+            self._store_state(context)
+
     def on_command(self, context: v1.deployment.Context, argv: [str]):
         """TODO"""
+
+    def region(self) -> str:
+        """TODO"""
+
+        return self.configuration["region"]
+
+    def add_object(self, obj: dict[str, object]):
+        """TODO"""
+
+        with self._lock:
+            name = f"{obj['kind']}/{obj['name']}"
+
+            if name in self._new_state:
+                raise RuntimeError(f"{name}: digital ocean object already exists")
+
+            self._new_state[name] = obj
+
+    def future(self, name: str, func: typing.Callable) -> v1.utils.Future:
+        """TODO"""
+
+        def resolve_future():
+            return func(self._current_state[name])
+
+        return v1.utils.Future(resolve_future)
 
 
 repository = {
--- torque/dolib.py
+++ torque/dolib.py
@@ -6,4 +6,156 @@ import difflib
 import re
 import sys
 
+import requests
 import yaml
+
+from torque import v1
+
+
+class Client:
+    """TODO"""
+
+    def __init__(self, endpoint: str, token: str):
+        self._endpoint = endpoint
+        self._session = requests.Session()
+
+        self._headers = {
+            "Authorization": f"Bearer {token}"
+        }
+
+    def post(self, path: str, json: object) -> object:
+        """TODO"""
+
+        return self._session.post(f"{self._endpoint}/{path}",
+                                  headers=self._headers,
+                                  json=json)
+
+
+    def put(self, path: str, json: object) -> object:
+        """TODO"""
+
+        return self._session.put(f"{self._endpoint}/{path}",
+                                 headers=self._headers,
+                                 json=json)
+
+    def get(self, path: str) -> object:
+        """TODO"""
+
+        return self._session.get(f"{self._endpoint}/{path}",
+                                 headers=self._headers)
+
+    def delete(self, path: str) -> object:
+        """TODO"""
+
+        return self._session.delete(f"{self._endpoint}/{path}",
+                                    headers=self._headers)
+
+
+class V2Certificates:
+    @staticmethod
+    def update(client: Client,
+               old_obj: dict[str, object],
+               new_obj: dict[str, object]) -> dict[str, object]:
+        """TODO"""
+
+        if old_obj:
+            raise RuntimeError(f"{old_obj['name']}: cannot update certificates")
+
+        res = client.post("v2/certificates", new_obj["params"])
+        data = res.json()
+
+        if res.status_code != 201:
+            raise RuntimeError(f"{new_obj['name']}: {data['message']}")
+
+        return {
+            "kind": new_obj["kind"],
+            "name": new_obj["name"],
+            "id": data["certificate"]["id"],
+            "params": new_obj["params"]
+        }
+
+
+    @staticmethod
+    def delete(client: Client, obj: dict[str, object]):
+        """TODO"""
+
+        client.delete(f"v2/certificates/{obj['id']}")
+
+
+HANDLERS = {
+    "v2/certificates": V2Certificates
+}
+
+
+def connect(endpoint: str, token: str) -> Client:
+    """TODO"""
+
+    return Client(endpoint, token)
+
+
+def _diff(name: str, obj1: dict[str, object], obj2: dict[str, object]):
+    """TODO"""
+
+    obj1 = yaml.safe_dump(obj1) if obj1 else ""
+    obj2 = yaml.safe_dump(obj2) if obj2 else ""
+
+    diff = difflib.unified_diff(obj1.split("\n"),
+                                obj2.split("\n"),
+                                fromfile=f"a/{name}",
+                                tofile=f"b/{name}",
+                                lineterm="")
+
+    diff = "\n".join(diff)
+
+    return obj1 != obj2, diff
+
+
+def apply(client: Client,
+          current_state: dict[str, object],
+          new_state: dict[str, object],
+          quiet: bool):
+    """TODO"""
+
+    for name, new_obj in new_state.items():
+        current_obj = current_state.get(name, None)
+        new_obj = v1.utils.resolve_futures(new_obj)
+
+        current_params = None
+        new_params = new_obj["params"]
+
+        if current_obj:
+            current_params = current_obj["params"]
+
+        changed, diff = _diff(name, current_params, new_params)
+
+        if not changed:
+            if not quiet:
+                print(f"{name}: no change", file=sys.stdout)
+
+            continue
+
+        if not quiet:
+            print(f"{name}: updating...", file=sys.stdout)
+
+        if not quiet:
+            print(diff, file=sys.stdout)
+
+        handler = HANDLERS[new_obj["kind"]]
+        current_state[name] = handler.update(client, current_obj, new_obj)
+
+    for name, current_obj in list(current_state.items()):
+        if name in new_state:
+            continue
+
+        if not quiet:
+            print(f"{name}: deleting...", file=sys.stdout)
+
+        _, diff = _diff(name, current_obj["params"], None)
+
+        if not quiet:
+            print(diff, file=sys.stdout)
+
+        handler = HANDLERS[current_obj["kind"]]
+        handler.delete(client, current_obj)
+
+        current_state.pop(name)
