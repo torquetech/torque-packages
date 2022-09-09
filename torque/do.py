dda6f1ce6fdd7a934a0212cb941e0453d53a1218
Jacob Zeba <jacob@torquetech.dev>
2022-09-09 02:39:01 +0000

    Convert dolib's setup_project() and setup_vpc() into regular objects

--- torque/do.py
+++ torque/do.py
@@ -11,6 +11,142 @@ from torque import v1
 from torque import dolib
 
 
+class _V2Projects:
+    """TODO"""
+
+    @classmethod
+    def _get_project(cls,
+                     client: dolib.Client,
+                     name: str) -> dict[str, object]:
+        """TODO"""
+
+        res = client.get("v2/projects")
+        data = res.json()
+
+        if res.status_code != 200:
+            raise RuntimeError(f"{name}: {data['message']}")
+
+        for project in data["projects"]:
+            if name == project["name"]:
+                return project
+
+        raise RuntimeError(f"unexpected error while looking for {name}")
+
+    @classmethod
+    def create(cls,
+               client: dolib.Client,
+               obj: dict[str, object]) -> dict[str, object]:
+        """TODO"""
+
+        res = client.post("v2/projects", obj["params"])
+        data = res.json()
+
+        if res.status_code != 201:
+            if res.status_code != 409:
+                raise RuntimeError(f"{obj['name']}: {data['message']}")
+
+            data = cls._get_project(client, obj["name"])
+
+        else:
+            data = data["project"]
+
+        return {
+            "kind": obj["kind"],
+            "name": obj["name"],
+            "metadata": {
+                "id": data["id"]
+            },
+            "params": obj["params"]
+        }
+
+    @classmethod
+    def update(cls,
+               client: dolib.Client,
+               old_obj: dict[str, object],
+               new_obj: dict[str, object]) -> dict[str, object]:
+        """TODO"""
+
+        raise RuntimeError(f"{old_obj['name']}: cannot update projects")
+
+    @classmethod
+    def delete(cls, client: dolib.Client, obj: dict[str, object]):
+        """TODO"""
+
+        client.delete(f"v2/projects/{obj['metadata']['id']}")
+
+    @classmethod
+    def wait(cls, client: dolib.Client, obj: dict[str, object]):
+        """TODO"""
+
+
+class _V2Vpcs:
+    """TODO"""
+
+    @classmethod
+    def _get_vpc(cls,
+                 client: dolib.Client,
+                 name: str) -> dict[str, object]:
+        """TODO"""
+
+        res = client.get("v2/vpcs")
+        data = res.json()
+
+        if res.status_code != 200:
+            raise RuntimeError(f"{name}: {data['message']}")
+
+        for vpc in data["vpcs"]:
+            if name == vpc["name"]:
+                return vpc
+
+        raise RuntimeError(f"unexpected error while looking for {name}")
+
+    @classmethod
+    def create(cls,
+               client: dolib.Client,
+               obj: dict[str, object]) -> dict[str, object]:
+        """TODO"""
+
+        res = client.post("v2/vpcs", obj["params"])
+        data = res.json()
+
+        if res.status_code != 201:
+            if res.status_code != 422:
+                raise RuntimeError(f"{obj['name']}: {data['message']}")
+
+            data = cls._get_vpc(client, obj["name"])
+
+        else:
+            data = data["vpc"]
+
+        return {
+            "kind": obj["kind"],
+            "name": obj["name"],
+            "metadata": {
+                "id": data["id"]
+            },
+            "params": obj["params"]
+        }
+
+    @classmethod
+    def update(cls,
+               client: dolib.Client,
+               old_obj: dict[str, object],
+               new_obj: dict[str, object]) -> dict[str, object]:
+        """TODO"""
+
+        raise RuntimeError(f"{old_obj['name']}: cannot update vpcs")
+
+    @classmethod
+    def delete(cls, client: dolib.Client, obj: dict[str, object]):
+        """TODO"""
+
+        client.delete(f"v2/vpcs/{obj['metadata']['id']}")
+
+    @classmethod
+    def wait(cls, client: dolib.Client, obj: dict[str, object]):
+        """TODO"""
+
+
 class Provider(v1.provider.Provider):
     """TODO"""
 
@@ -45,9 +181,6 @@ class Provider(v1.provider.Provider):
         self._params = None
         self._client = None
 
-        self._project = None
-        self._vpc = None
-
         self._current_state = {}
         self._new_state = {}
 
@@ -55,6 +188,25 @@ class Provider(v1.provider.Provider):
         self._load_state()
 
         self._connect()
+
+        self.add_object({
+            "kind": "v2/projects",
+            "name": self._params["project_name"],
+            "params": {
+                "name": self._params["project_name"],
+                "purpose": "torquetech.io deployment",
+                "environment": "Production"
+            }
+        })
+
+        self.add_object({
+            "kind": "v2/vpcs",
+            "name": self._params["vpc_name"],
+            "params": {
+                "name": self._params["vpc_name"],
+                "region": self._params["region"]
+            }
+        })
 
     def _check_params(self):
         if not self._params:
@@ -93,15 +245,11 @@ class Provider(v1.provider.Provider):
 
         self._check_params()
 
-        self._project = dolib.setup_project(self._client, self._params["project_name"])
-        self._vpc = dolib.setup_vpc(self._client, self._params["vpc_name"], self._params["region"])
-
         try:
             wait_hooks = dolib.apply(self._client,
                                      self._current_state,
                                      self._new_state,
                                      self.configuration["quiet"])
-
 
             self._post_apply_hooks.extend(wait_hooks)
 
@@ -155,30 +303,6 @@ class Provider(v1.provider.Provider):
 
         return self._client
 
-    def _resolve_project_id(self):
-        """TODO"""
-
-        if not self._project:
-            return "<project_id>"
-
-        return self._project["id"]
-
-    def _resolve_vpc_id(self):
-        """TODO"""
-
-        if not self._vpc:
-            return "<vpc_id>"
-
-        return self._vpc["id"]
-
-    def _resolve_region(self):
-        """TODO"""
-
-        if not self._params:
-            return "<region>"
-
-        return self._params["region"]
-
     def _resolve_object_id(self, name: str) -> str:
         """TODO"""
 
@@ -187,25 +311,25 @@ class Provider(v1.provider.Provider):
 
         return self._current_state[name]["metadata"]["id"]
 
-    def project_id(self) -> v1.utils.Future[str]:
-        """TODO"""
-
-        return v1.utils.Future(self._resolve_project_id)
-
-    def vpc_id(self) -> v1.utils.Future[str]:
-        """TODO"""
-
-        return v1.utils.Future(self._resolve_vpc_id)
-
     def object_id(self, name: str) -> v1.utils.Future[str]:
         """TODO"""
 
         return v1.utils.Future(functools.partial(self._resolve_object_id, name))
 
+    def project_id(self) -> v1.utils.Future[str]:
+        """TODO"""
+
+        return self.object_id(f"v2/projects/{self._params['project_name']}")
+
+    def vpc_id(self) -> v1.utils.Future[str]:
+        """TODO"""
+
+        return self.object_id(f"v2/vpcs/{self._params['vpc_name']}")
+
     def region(self) -> str:
         """TODO"""
 
-        return v1.utils.Future(self._resolve_region)
+        return self._params["region"]
 
     def add_object(self, obj: dict[str, object]):
         """TODO"""
@@ -218,6 +342,11 @@ class Provider(v1.provider.Provider):
 
             self._new_state[name] = obj
 
+
+dolib.HANDLERS.update({
+    "v2/projects": _V2Projects,
+    "v2/vpcs": _V2Vpcs
+})
 
 repository = {
     "v1": {
